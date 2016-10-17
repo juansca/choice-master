7e82f50812c48fd7a080fd346dcf2fedf0079cc1
Francisco Trucco <franciscoctrucco@gmail.com>
2016-10-17 01:41:28 -0300

    index now redirects to login when not logged in.
    From the specification:
        "El usuario Logueado debe poder ejecutar la acción logout para salir
        del sistema. Luego de realizada la acción no podrá acceder a
        funciones del sistema y será redirigido nuevamente a la pantalla de
        Login"
    thus, in order for this to work properly we must redirect the user to
    the login screen.

--- choice_master/chm/views.py
+++ choice_master/chm/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from allauth.account.views import login
 
 def index(request):
     context = {'request': request}
+    if not request.user.is_authenticated:
+        return redirect(login)
     return render(request, 'index.html')
