2514c2311bcbb7701d975716b0493d238b429be9
juan_sca <juansca1229@gmail.com>
2016-10-17 15:04:29 -0300

    Change language and some changes in nav

--- choice_master/choice_master/settings.py
+++ choice_master/choice_master/settings.py
@@ -140,7 +140,7 @@ AUTHENTICATION_BACKENDS = (
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
