d94e7ca0a6f0a74d59c7306c67d066cc12b8bfe0
Marcos Modenesi <marcosmodenesi@gmail.com>
2016-10-23 17:23:00 -0300

    initial implementations of models for app chm

--- choice_master/chm/models.py
+++ choice_master/chm/models.py
@@ -1,7 +1,47 @@
-# from django.db import models
+from django.db import models
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 from django.contrib import messages
+from django.contrib.auth.models import User
+
+
+class Subject(models.Model):
+    """
+    """
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+
+
+class Topic(models.Model):
+    """
+    """
+    name = models.CharField(max_length=200)
+    subject = models.ForeignKey('Subject')
+
+
+class Question(models.Model):
+    """
+    """
+    topic = models.ForeignKey('Topic')
+    text = models.CharField(max_length=300)
+
+
+class Answer(models.Model):
+    """
+    """
+    text = models.CharField(max_length=300)
+    question = models.ForeignKey('Question')
+    is_correct = models.BooleanField(default=False)
+
+
+class Flag(models.Model):
+    """
+    A question flagged by a user
+    """
+    question = models.ForeignKey('Question')
+    datetime = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    description = models.TextField()
 
 
 @receiver(user_signed_up)
