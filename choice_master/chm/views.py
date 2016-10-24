791a0f5784bfb52ba1e2175c24876ee24787edd0
Francisco Trucco <franciscoctrucco@gmail.com>
2016-10-24 19:57:42 -0300

    Implemented the view Upload.
    If the user sends a GET request, this view will render a template
    allowing the user to send a form.
    If the user sends a POST request, this view will load the questions in
    the database only if they are not already present. After loading all the
    questions that can be loaded, the view renders a list of all the
    questions that were saved and all the ones that weren't and why. If the
    xml provided by the user is not valid the user will be warned and a
    brief description explaining how the file should look like will be displayed
    using xmlformathelp.html. If the question is too similar according to
    the similarity function provided in similarity.py, then the question is
    not saved and the user is warned.
    
    Problems and some possible improvements:
    - xmlformathelp is too simple and no specific information about the
      failure is given.
    - similarity is not yet fully implemented, thus the similarity function
      will always return False. For now.

--- choice_master/chm/views.py
+++ choice_master/chm/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from allauth.account.views import login
+from django.contrib import messages
 
+from lxml.etree import XMLSyntaxError
 from chm.forms import XMLFileForm
+from chm.models import Topic
+from chm.xml import parse_questions
+
+from chm import similarity
 
 
 def index(request):
@@ -13,49 +19,47 @@ def index(request):
         return redirect(login)
     return render(request, 'index.html')
 
-"""
-def upload(request):
-    context = {'topics': Topic.objects.all(), 'subjects': Subject.objects.all()}
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile=request.FILES['docfile'])
-            newdoc.save()
 
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('upload'))
-    else:
-        form = DocumentForm()  # A empty, unbound form
-
-    # Load documents for the list page
-    documents = Document.objects.all()
-    return render(request, 'upload.html', context)
-"""
+def extract_topic_and_subject_from_data(data):
+    topic_and_subject = str(data['topic'])
+    subject = ' (' + str(data['subject']) + ')'
+    topic = topic.replace(subject, '')
+    return topic, subject
 
 
 def upload(request):
-    context = {'questions_added': [], 'questions_errors': []}
+    context = {'added': [],
+               'repeated': [],
+               'similar_exists': []}
+
     if request.method == 'POST':
         form = XMLFileForm(request.POST, request.FILES)
         if form.is_valid():
-            for question in f(request.FILES['XMLfile']):
-                if not_repeated(question):
-                    ## salvar en la bbdd
-                    context['questions_added'].append(question)
-                else:
-                    context['questions_errors'].append(question)
+            data = form.cleaned_data
 
+            try:
+                xmlfile = request.FILES['XMLfile']
+                for question, answers in parse_questions(xmlfile):
+                    topic, subject = extract_topic_and_subject_from_data(data)
+                    question.topic = Topic.objects.get(name=topic)
+
+                    if similarity.repeated(question):
+                        context['repeated'].append(question)
+                    elif similarity.similar_exists(question):
+                        context['similar_exists'].append(question)
+                    else:
+                        context['added'].append(question)
+
+                        question.save()
+                        for ans in answers:
+                            ans.question = question
+                            ans.save()
+
+            except XMLSyntaxError as err:
+                context['syntax_error'] = err
     else:
         form = XMLFileForm()
 
     context['form'] = form
     # Load documents for the list page
     return render(request, 'upload.html', context)
-
-### JUANSCA Y TRUCCO: linkear acá
-def f(*args):
-    return '42'
-
-### JUANSCA Y TRUCCO: linkear acá
-def not_repeated(*args):
-    return True
