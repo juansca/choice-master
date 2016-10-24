c48fa874bc32af3c8e7a29a5cae8975b0cf7de05
Francisco Trucco <franciscoctrucco@gmail.com>
2016-10-24 19:53:41 -0300

    Style changes. Nothing to comment.

--- choice_master/chm/forms.py
+++ choice_master/chm/forms.py
@@ -4,7 +4,6 @@ from chm.models import Subject
 
 
 class XMLFileForm(forms.Form):
-
     subject = forms.ModelChoiceField(queryset=Subject.objects.all())
     topic = forms.ModelChoiceField(queryset=Topic.objects.all())
     XMLfile = forms.FileField(
--- choice_master/chm/xml.py
+++ choice_master/chm/xml.py
@@ -1,31 +1,31 @@
-from lxml.etree import XMLSchema
-from lxml.etree import iterparse
-from lxml.etree import parse
-from chm.models import Question
-from chm.models import Answer
+from os import path
+from lxml import etree
+from chm import models
 from choice_master.settings import BASE_DIR
-import os
 
-xml_schema_path = os.path.join(BASE_DIR, 'static', 'xml_files', 'question.xml')
+xmlschema_path = path.join(BASE_DIR, 'static', 'xml_files', 'question.xml')
+
 
 def get_schema():
-    with open(xml_schema_path, "r") as g:
-        xml_schema_doc = parse(g)
-    xml_schema = XMLSchema(xml_schema_doc)
-    return xml_schema
+    with open(xmlschema_path, "r") as g:
+        xmlschema_doc = parse(g)
+    xmlschema = etree.XMLSchema(xmlschema_doc)
+    return xmlschema
 
-def load_questions(xml_file):
+
+def parse_questions(xmlfile):
     schema = get_schema()
-    for _, question_data in iterparse(xml_file, tag='question', schema=schema):
-        question = Question()
-        question.text = question_data[0].text
+    for _, question_data in etree.iterparse(xmlfile,
+                                            tag='question',
+                                            schema=schema):
         answers = []
+        question = models.Question()
+        question.text = question_data[0].text
 
         for i in range(1, len(question_data)):
-            answer = Answer(text=question_data[i].text,
-                            question=question,
-                            is_correct=(question_data[i].tag is "correct"))
+            text = question_data[i].text
+            is_correct = question_data[i].tag is "correct"
+            answer = models.Answer(text=text, is_correct=is_correct)
             answers.append(answer)
 
-        question_data.clear()
         yield question, answers
