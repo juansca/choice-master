ceb89ea6b0032fae12e2084b8e0076baf80a864f
Francisco Trucco <franciscoctrucco@gmail.com>
2016-10-24 22:37:07 -0300

    Style changes. Nothing to comment.

--- choice_master/chm/similarity.py
+++ choice_master/chm/similarity.py
@@ -2,18 +2,22 @@ from chm.models import Question
 
 
 def distance(str1, str2):
-  distance_table = dict()
-  for i in range(len(str1) + 1):
-     distance_table[i] = dict()
-     distance_table[i][0] = i
-  for i in range(len(str2) + 1):
-     distance_table[0][i] = i
-  for i in range(1, len(str1) + 1):
-     for j in range(1, len(str2) + 1):
-        distance_table[i][j] = min(distance_table[i][j - 1] + 1,
-        distance_table[i - 1][j] + 1,
-        distance_table[i - 1][j - 1] + (not str1[i - 1] == str2[j - 1]))
-  return distance_table[len(str1)][len(str2)]
+    distance_table = dict()
+
+    for i in range(len(str1) + 1):
+        distance_table[i] = dict()
+        distance_table[i][0] = i
+
+    for i in range(len(str2) + 1):
+        distance_table[0][i] = i
+
+    for i in range(1, len(str1) + 1):
+        for j in range(1, len(str2) + 1):
+            distance_table[i][j] = min(distance_table[i][j - 1] + 1,
+                                       distance_table[i - 1][j] + 1,
+                                       distance_table[i - 1][j - 1] +
+                                       (not str1[i - 1] == str2[j - 1]))
+    return distance_table[len(str1)][len(str2)]
 
 
 def is_similar(str1, str2):
@@ -21,7 +25,8 @@ def is_similar(str1, str2):
 
 
 def repeated(pquestion):
-    return Question.objects.filter(text=pquestion.text, topic=pquestion.topic).exists()
+    return Question.objects.filter(text=pquestion.text,
+                                   topic=pquestion.topic).exists()
 
 
 def similar_exists(pquestion):
@@ -33,4 +38,3 @@ def similar_exists(pquestion):
         if some_similar:
             break
     return some_similar
-
