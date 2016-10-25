e5f692139e82042890434ad8362492202c96817b
Francisco Trucco <franciscoctrucco@gmail.com>
2016-10-24 22:26:58 -0300

    Similarity now only compares between questions of the same topic.

--- choice_master/chm/similarity.py
+++ choice_master/chm/similarity.py
@@ -1,5 +1,6 @@
 from chm.models import Question
 
+
 def distance(str1, str2):
   distance_table = dict()
   for i in range(len(str1) + 1):
@@ -14,16 +15,19 @@ def distance(str1, str2):
         distance_table[i - 1][j - 1] + (not str1[i - 1] == str2[j - 1]))
   return distance_table[len(str1)][len(str2)]
 
+
 def is_similar(str1, str2):
     return distance(str1, str2) < 5
 
 
 def repeated(pquestion):
-    return Question.objects.filter(text=pquestion.text).exists()
+    return Question.objects.filter(text=pquestion.text, topic=pquestion.topic).exists()
+
 
 def similar_exists(pquestion):
+    topic_questions = Question.objects.filter(topic=pquestion.topic)
     some_similar = False
-    for db_question in Question.objects.values('text'):
+    for db_question in topic_questions.values('text'):
         dbqtext = db_question['text']
         some_similar = some_similar or is_similar(pquestion.text, dbqtext)
         if some_similar:
